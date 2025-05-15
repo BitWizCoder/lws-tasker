509bbe35028247587c975cb29fcba6d7b1157477
Muhammad Noman <mhmdnoman01@gmail.com>
2025-05-15 21:26:07 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
+    </>
+  );
+}
